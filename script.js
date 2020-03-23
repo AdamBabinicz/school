4407654717978f38dc30d4e078b08e3aba5984fb
hipek <puaro@vp.pl>
2020-03-23 18:38:26 +0100

    zmiany w script i css

--- script.js
+++ script.js
@@ -2,29 +2,25 @@ document.querySelector(".card-btn").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
 
-let box1 = document.querySelector(".box1");
-let box2 = document.querySelector(".box2");
-let box3 = document.querySelector(".box3");
-let check1 = document.querySelector(".check1");
-let check2 = document.querySelector(".check2");
-let check3 = document.querySelector(".check3");
+// let box1 = document.querySelector(".box1");
+// let box2 = document.querySelector(".box2");
+// let box3 = document.querySelector(".box3");
+// let check1 = document.querySelector(".check1");
+// let check2 = document.querySelector(".check2");
+// let check3 = document.querySelector(".check3");
 
-check1.addEventListener("click", () => {
-  box1.classList.toggle("open");
-});
-check2.addEventListener("click", () => {
-  box2.classList.toggle("open");
-});
-check3.addEventListener("click", () => {
-  box3.classList.toggle("open");
-});
-// let check = document.querySelector(".check");
-// let box = document.querySelectorAll(".box");
-
-// box.forEach(el => {
-//   el.addEventListener("click", () => {
-//     document.querySelector(".container").classList.remove("open");
-
-//     el.classList.add("open");
-//   });
+// check1.addEventListener("click", () => {
+//   box1.classList.toggle("open");
 // });
+// check2.addEventListener("click", () => {
+//   box2.classList.toggle("open");
+// });
+// check3.addEventListener("click", () => {
+//   box3.classList.toggle("open");
+// });
+
+for (let i = 1; i <= 3; i++) {
+  document.querySelector(`.check${i}`).addEventListener("click", () => {
+    document.querySelector(`.box${i}`).classList.toggle("open");
+  });
+}
