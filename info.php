4489608bcf88878ecc21a97c45ac8212333c6bfe
babinicz <babinicz@onet.pl>
2021-09-09 17:46:38 +0200

    zmiana serwera

--- info.php
+++ info.php
@@ -1,5 +1,2 @@
-
-
 <?php
 echo __FILE__;
-?>
--- script.js
+++ script.js
@@ -2,23 +2,6 @@ document.querySelector(".card-btn").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
 
-// let box1 = document.querySelector(".box1");
-// let box2 = document.querySelector(".box2");
-// let box3 = document.querySelector(".box3");
-// let check1 = document.querySelector(".check1");
-// let check2 = document.querySelector(".check2");
-// let check3 = document.querySelector(".check3");
-
-// check1.addEventListener("click", () => {
-//   box1.classList.toggle("open");
-// });
-// check2.addEventListener("click", () => {
-//   box2.classList.toggle("open");
-// });
-// check3.addEventListener("click", () => {
-//   box3.classList.toggle("open");
-// });
-
 // const arrows = []; // document.querySelectorAll Pobierasz wszystkie checki
 // const boxes = []; // document.querySelectorAll Pobierasz wszystkie boxy
 
